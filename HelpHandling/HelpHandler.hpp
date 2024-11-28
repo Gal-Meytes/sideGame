662b66d9affc5f76f4e3056494c70f5f353093d8
username <email.com>
2024-11-28 23:30:28 +0200

    Add documentation for HelpHandler implementation and header files

--- HelpHandling/HelpHandler.hpp
+++ HelpHandling/HelpHandler.hpp
@@ -1,7 +1,28 @@
+
+/**
+ * @file HelpHandler.hpp
+ * @brief Header file for help handling functions.
+ *
+ * This file contains the declarations of functions used for handling help-related
+ * functionalities in the application.
+ */
 #ifndef HELP_HANDLER_H
 #define HELP_HANDLER_H
 
+/**
+ * @brief Handles the help command.
+ *
+ * This function is responsible for processing and executing the help command
+ * provided by the user.
+ */
 void handleHelp();
+
+/**
+ * @brief Displays help information.
+ *
+ * This function displays the help information to the user, providing guidance
+ * on how to use the application.
+ */
 void help();
 
 #endif // HELP_HANDLER_H
